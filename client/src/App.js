1f470269c09c60165ef99acea6f52d5e71ec850e
andi balo <andibalo213@gmail.com>
2020-03-13 17:40:38 +0700

    finished backend routes and set up inital react components

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import './App.css';
+
+//We want the page aside from landing to be inside a container so the routes with compo we wrap within a section with ckass container
+
+const App = () => (
+  <Router>
+    <Fragment >
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+)
+
+export default App;
