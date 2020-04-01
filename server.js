b2b7ab54ad566c3828f9e01dc443a4daa0e8ac8f
andi balo <andibalo213@gmail.com>
2020-04-01 21:44:43 +0700

    prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 //DATABASE CONNECTION
 const connectDB = require('./config/db')
@@ -22,12 +23,19 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/post'))
 
+//Serve static assest in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`)
 })
 
-app.get('/', (req, res) => {
-    res.send('servesr started')
-})
